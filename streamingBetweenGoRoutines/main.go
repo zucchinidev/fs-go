fdd9e01ab3fc5b3677d33fea94bf371ea42c4964
zucchinidev <azga84@gmail.com>
2020-12-17 13:36:26 +0100

    This commit will add a script so as to send data between goroutines using streams and the built-in function pipe

---
+++ streamingBetweenGoRoutines/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+func main() {
+	reader, writer := io.Pipe()
+
+	go func(w io.WriteCloser) {
+		for _, s := range []string{"communicating streams", " using go routines ", " with the built-in ", " pipe function "} {
+			fmt.Printf("-> writing %q\n", s)
+			_, _ = fmt.Fprint(w, s)
+		}
+		w.Close() // we need to close the writer so as to send EOF error when a reader tries to read more data
+	}(writer)
+
+	var err error
+	for n, b := 0, make([]byte, 100); err == nil; {
+		fmt.Println("<- waiting...")
+		n, err = reader.Read(b)
+		if err == nil {
+			fmt.Printf("<- received %q\n", string(b[:n]))
+		}
+	}
+
+	if err != io.EOF {
+		fmt.Println("error:", err)
+	}
+}
